6b1f3dd2b88760823ade0219d8ed658eb426b4a7
TranBaBang16 <tranbabang16@gmail.com>
2021-12-03 11:38:15 +0700

    Update Pic.go

--- Pic.go
+++ Pic.go
@@ -1,5 +1,6 @@
 
-//Implement Pic. It should return a slice of length dy, each element of which is a slice of dx 8-bit unsigned integers. When you run the program, it will display your picture, interpreting the integers as grayscale (well, bluescale) values.
+//Implement Pic. It should return a slice of length dy, each element of which is a slice of dx 8-bit unsigned integers. 
+//When you run the program, it will display your picture, interpreting the integers as grayscale (well, bluescale) values.
 
 //The choice of image is up to you. Interesting functions include (x+y)/2, x*y, and x^y.
 
@@ -13,13 +14,12 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
-		arr:=make([][]uint8,dy)
-		for i:=range arr{
-			arr[i]=make([]uint8,dx)
-			for j:=range arr[i]{
-				arr[i][j] =uint8(i*j)
-				}
+	arr := make([][]uint8,dy)
+	for i := range arr {
+		arr[i] = range arr[i]{
+			arr[i][j] = uint8(i*j)
 		}
+	}
 	return arr
 	
 }
