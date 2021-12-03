363c0da5035f716cc26057d95cec85fdffe6a454
TranBaBang16 <tranbabang16@gmail.com>
2021-12-03 11:47:49 +0700

    Update Sqrt.go

--- Sqrt.go
+++ Sqrt.go
@@ -7,11 +7,13 @@ package main
 import (
 	"fmt"
 )
-const m float64 =0.0000000000000001
+const m float64 = 0.0000000000000001
 func Sqrt(x float64) float64 {
 	z:=float64(1)
-for (z*z-x)*(z*z-x)>m {
-z -= (z*z - x) / (2*z)
+// so sanh voi m de vong lap dung lai khi ma z dat den gia tri ma sai so so voi sqrt(x) la chap nhan duoc
+//trong bai nay em lay m=0.1*10^-16 => ket qua chinh xac den 8 chu so thap phan
+	for (z*z-x)*(z*z-x) > m {
+	z -= (z*z - x) / (2*z)
 	}
 	return z
 }
@@ -19,3 +21,5 @@ z -= (z*z - x) / (2*z)
 func main() {
 	fmt.Println(Sqrt(7))
 }
+
+
