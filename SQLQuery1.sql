8822089e403251835cd9ab8ad9dffb3223453840
TranBaBang16 <tranbabang16@gmail.com>
2021-12-22 17:51:48 +0700

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,91 +1,93 @@
-﻿--Tạo bảng
-Create database bang_test
+-- Tạo bảng 
+Create database bang_test ;
 use bang_test;
-create table Project_user (
-id bigint not null Identity(1,1) primary key,
+create table Project_users (
+id bigint not null primary key auto_increment,
 project_id bigint not null ,
 user_id bigint not null,
-created_at timestamp ,
-updated_at date
+created_at timestamp default current_timestamp not null,
+updated_at timestamp default current_timestamp not null
  );
 
 create table Project (
-id bigint not null Identity(1,1) primary key,
+id bigint not null auto_increment primary key,
 project_name varchar(50) not null,
 category_id  bigint  not null,
 project_spend int not null,
 project_variance int not null,
 revenue_recognised int not null,
-created_at timestamp not null,
-updated_at date not null,
-company_id bigint
-
+created_at timestamp default current_timestamp not null,
+updated_at  timestamp default current_timestamp not null,
+company_id bigint not null 
 );
 create table users (
-id bigint not null Identity(1,1) primary key,
+id bigint not null auto_increment primary key,
 user_name varchar(50) not null,
 email varchar(100) not null,
 pass_word varchar(50) not null,
 birthday date not null ,
 image_url text not null,
-is_active bit not null,
-created_at timestamp not null,
-updated_at date not null,
+is_active bool not null,
+created_at timestamp default current_timestamp not null,
+updated_at timestamp default current_timestamp not null
 );
-use bang_test
-ALTER TABLE Project_user
-add  foreign key (project_id) references project (id);
 
-ALTER TABLE Project_user
-add  foreign key (user_id) references users (id);
+ALTER TABLE Project_users
+add  constraint project_users_users
+foreign key (project_id) references project (id);
+
+ALTER TABLE Project_users
+add  constraint project_users_users_id
+foreign key (user_id) references users (id);
 
 create table categories (
-id bigint not null primary key identity(1,1),
+id bigint not null primary key auto_increment,
 category_name varchar(50) not null,
-created_at timestamp not null,
-updated_at date not null
-)
+created_at timestamp default current_timestamp not null,
+updated_at timestamp default current_timestamp not null
+);
 
 create table companies (
-id bigint not null primary key identity(1,1),
+id bigint not null primary key auto_increment,
 company_name varchar(100) not null,
 company_code varchar(100) not null,
-created_at timestamp not null,
-updated_at date not null
-)
+created_at timestamp default current_timestamp not null,
+updated_at timestamp default current_timestamp not null
+);
 
 alter table companies
-alter column company_name varchar(50) not null
+modify column company_name varchar(50) not null;
 
 alter table users
-alter column email varchar(50) not null
+modify column email varchar(50) not null;
 
 alter table users
-alter column user_name varchar(16) not null
+modify column user_name varchar(16) not null;
 
 alter table users
-alter column pass_word varchar(10) not null
+modify column pass_word varchar(10) not null;
 
 alter table project
-add foreign key (category_id) references categories(id);
+add constraint project_category_fk
+foreign key (category_id) references categories(id);
 
 alter table project
-add foreign key (company_id) references companies(id);
+add constraint project_companies_fk foreign key (company_id) references companies(id);
 
 alter table project
-alter column project_spend int
+modify column project_spend int;
 
 alter table project
-alter column project_variance int
+modify column project_variance int;
 
 alter table project
-alter column revenue_recognised int
+modify column revenue_recognised int;
 
 alter table users
-alter column birthday date
+modify column birthday date;
 
 alter table users
-alter column image_url varchar
+modify column image_url varchar(100) ;
 
 
 -- CHèn dữ liệu vào bảng
@@ -95,32 +97,33 @@ user_name,
 email,
 pass_word,
 birthday,
-is_active,
-created_at,
-updated_at
+is_active
 )
 values (
-'Dao van B',
-'yyyyyyyyyyy@gmail.com',
+'Tran B',
+'ttbbbbb@gmail.com',
 '12345678',
-'2002/12/23',
-1,
-DEFAULT,
-'2021/12/23'
-)
+'1997/01/16',
+true
+);
+
+select * from users;
 
 Insert into companies(
 company_name,
-company_code,
-created_at,
-updated_at
+company_code
 )
 values (
-'monstar-lab',
-'1234',
-default,
-'2011-11-01'
+'lll-lab',
+'1122'
+);
+
+Insert into categories(
+category_name
 )
+values (
+'mmm'
+);
 
 insert into Project (
 project_name,
@@ -128,85 +131,77 @@ category_id,
 project_spend,
 project_variance,
 revenue_recognised,
-created_at,
-updated_at,
 company_id
 )
 values (
-'project_4',
-2,
+'project_5',
+4,
 132,
 123,
 23,
-default,
-'2021/05/30',
-2
-)
+4
+);
 
-insert into Project_user (
+delete from project
+where project_name = 'project_3' and category_id=3;
+
+insert into Project_users (
 project_id,
-user_id,
-created_at,
-updated_at
+user_id
 )
 values (
-10,
-3,
-default,
-'2020/12/05'
-)
+6,
+2
+);
+select * from project_users;
+select * from companies;
+select * from categories;
 
---Viết lệnh sql để có thể lấy thông tin những bản ghi của projects 
---và số lượng user của mỗi projects đó (count user)
-select project.id,project_name,project_spend,Project.project_variance,Project.category_id, count(users.id) as "count_user" from Project
-left join project_user on Project.id= Project_user.project_id
-left join users on Project_user.user_id=USERs.id
-group by project.id,project_name,project_spend,Project.project_variance,Project.category_id
+-- Viết lệnh sql để có thể lấy thông tin những bản ghi của projects 
+-- và số lượng user của mỗi projects đó (count user)
+
+select Project.* ,count(project_users.user_id) as "count_user" from Project
+inner join project_users on Project.id = Project_users.Project_id
+group by id ;
 
 
---viết lệnh sql để lấy ra danh sách các project của company có company_name = “monstar-lab” 
+-- viết lệnh sql để lấy ra danh sách các project của company có company_name = “monstar-lab” 
 
 select * from Project
 inner join companies
-on Project.company_id=companies.id
-where company_name = 'monstar-lab'
+on Project.company_id = companies.id
+where company_name = 'monstar-lab';
 
---viết lệnh sql lấy ra danh sách các công ty có project có project_spend > 100
+-- viết lệnh sql lấy ra danh sách các công ty có project có project_spend > 100
 
-select companies.company_name from companies
+select distinct companies.company_name from companies
 inner join Project
 on Project.company_id=companies.id
-where project.project_spend>100
+where project.project_spend>100;
 
---viết lệnh sql để lấy ra thông tin của user tham gia vào projects
-select distinct USERs.id,user_name,email,pass_word,birthday,image_url,USERs.created_at,USERs.updated_at from users 
-inner join Project_user
-on users.id=Project_user.user_id
-inner join Project
-on Project_user.project_id=Project.id
+-- viết lệnh sql để lấy ra thông tin của user tham gia vào projects
+select distinct USERs.* from users 
+inner join Project_users on users.id = Project_users.user_id
+group by id;
 
---lấy ra danh sách project mà có số lượng user tham gia > 10 , sắp xếp số lượng user tham gia tăng dần
+-- lấy ra danh sách project mà có số lượng user tham gia > 10 , sắp xếp số lượng user tham gia tăng dần
 
-select project_name,
-count(users.id) as "count_user" 
+select project_name, count(user_id) as "count_user" 
 from Project
-left join project_user on Project.id= Project_user.project_id
-left join users on Project_user.user_id=USERs.id
-group by project_name
-having count(users.id)>10
-order by count(users.id) asc
+inner join project_users on Project.id= Project_users.project_id
+group by Project_name
+having count_user > 10
+order by count_user asc;
 
 
---Xoá project mà chưa có user nào tham gia
-use bang_test
+-- Xoá project mà chưa có user nào tham gia
+use bang_test;
+select * from project;
+
 delete from project 
-where project_name = (
-select project_name
-from Project
-left join project_user on Project.id= Project_user.project_id
-left join users on Project_user.user_id=USERs.id
-group by Project.project_name
-having count(users.id)=0
+where project_name not in (
+select distinct project_name from(
+select distinct project.* from Project
+inner join project_users on Project.id = Project_users.project_id
+) as t
 )
-
-
